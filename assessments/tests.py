45537f2fa342da876a31f1cf438bd6474610be0b
Jackeline Geraldine Brooks Marcano <zy238386@ou.ac.uk>
2026-09-25 13:14:25 +0000

    test: add assessment auth and threat list tests

--- assessments/tests.py
+++ assessments/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class AssessmentTests(TestCase):
+    def test_assessment_form_requires_login(self):
+        response = self.client.get(reverse("assessment-form", args=[1]))
+        self.assertEqual(response.status_code, 302)
--- threats/tests.py
+++ threats/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class ThreatTests(TestCase):
+    def test_threat_list_ok(self):
+        response = self.client.get(reverse("threat-list"))
+        self.assertEqual(response.status_code, 200)
